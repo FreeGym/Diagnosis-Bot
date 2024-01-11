375143d88f6d5134c457bf42392605ec22b03989
Pranav <pranav.sai0809@gmail.com>
2024-01-11 21:54:05 +0530

    WIP: Medical Agents Group chat functionality

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import os
 from dotenv import load_dotenv, find_dotenv
 from autogen.agentchat.contrib.retrieve_assistant_agent import RetrieveAssistantAgent
 from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
-# import chromadb
 import chromadb.utils.embedding_functions as embedding_functions
-# import openai
+import autogen
+import json
+from utils import to_snake_case
 
 _ = load_dotenv(find_dotenv()) 
 
@@ -13,19 +14,18 @@ openai_ef = embedding_functions.OpenAIEmbeddingFunction(
                 model_name="text-embedding-ada-002"
             )
 
-
 config_list = [
     {
           "model" : "gpt-3.5-turbo",
-          "api_key" : os.getenv('OPENAI_API_KEY'),
-          "max_tokens" : 1000
-    }
-  
+          "api_key" : os.getenv("OPENAI_API_KEY"),
+          "max_tokens" : 1000,
+          "temperature" : 0.3
+    }  
 ]
 
-
-compounderProxy = RetrieveUserProxyAgent(
-    name = "Compounder Proxy",
+agents = {}
+agents["compounderProxy"] = RetrieveUserProxyAgent(
+    name = "CompounderProxy",
     retrieve_config={
         "task" : "qa",
         "docs_path": "docs/",
@@ -35,27 +35,63 @@ compounderProxy = RetrieveUserProxyAgent(
         "embedding_function" : openai_ef,
         "get_or_create" : True,
     },
+    human_input_mode="TERMINATE",
     code_execution_config = False
 )
 
-compounderAgent = RetrieveAssistantAgent(
-    name = "Compounder Agent",
+
+with open('medical_professionals.json', 'r') as file:
+    data = json.load(file)
+sports_medicine_specialists = data['sports_medicine_specialists']
+
+
+
+agents["compounder"]= RetrieveAssistantAgent(
+    name = "Compounder",
     description = "compounder: interviews patients about their health using a specified framework",
     system_message = """You are a compounder agent. \
                         Your job is to use a framework given in the context to ask patient questions pertaining to their health.\
-                        In case the patient doesn't ask anything related to sickness reply `YOU ARE HEALTHY , HAVE FUN`
+                        Remember to only ask ONE QUESTION AT A TIME.\
+                        Once you obtain primary symptom information from the patient, you present the case to Specialist Medical Agents in the group chat.\
+                        At the end your job is to obtain the final diagnosis and convey the same to the patient.
                         If the context is irrelevant to the patient's query reply `I DON'T KNOW`
                         """,
-    human_input_mode = "TERMINATE",
+    human_input_mode = "ALWAYS",
     llm_config = {
         "timeout": 600,
         "cache_seed": 42,
         "config_list": config_list,
-    }
-    
+        "max_retries" : 10
+    }   
 )
 
-prompt = "I am not feeling well for the past 2 days. Can you please help me ?"
+for sports_medicine_specialist in sports_medicine_specialists:
+    agents[sports_medicine_specialist["name"]] = RetrieveAssistantAgent(
+        name = to_snake_case(sports_medicine_specialist["name"]),
+        description = sports_medicine_specialist["name"] + " : " + sports_medicine_specialist["description"],
+        system_message = f"""
+                        You are a {sports_medicine_specialist["name"]} and you specialise in {sports_medicine_specialist["description"]}.\
+                        Your job is to convey the final diagnosis to the COMPOUNDER.
+                        You may ask the patient clarifying questions regarding their symptoms. ASK ONE QUESTION AT A TIME. 
+                        You may also converse with other medical experts in the group chat before arriving at the final diagnosis""",
+        human_input_mode = "ALWAYS",
+        llm_config = {
+            "timeout": 600,
+            "cache_seed": 42,
+            "config_list": config_list,
+            "max_retries" : 10
+        }   
+    )
+
+def _reset_agents():
+    for agent in agents.keys():
+        agents[agent].reset()
+
+problem = input("Hey there , welcome to diagnosis bot? What kind of health issues are you facing ?\n" + "-"*100 +"\n")
+# prompt = "I have knee pain"
+# prompt = "What colour is the sky?" # Test prompt for checking hallucinations
+prompt=f"""Please help with the diagnosis of the issues delimited by triple backticks 
+        ```{problem}```"""
 termination_notice = (
     '\n\nDo not show appreciation in your responses, say only what is necessary. '
     'if "Thank you" or "You\'re welcome" are said in the conversation, then say TERMINATE '
@@ -63,4 +99,29 @@ termination_notice = (
 )
 prompt += termination_notice
 
-compounderProxy.initiate_chat(compounderAgent,problem=prompt,search_string="symptoms")
+def _reset_agents() :
+    for agent in agents.keys():
+        agents[agent].reset()
+
+llm_config = {
+            "timeout": 600,
+            "cache_seed": 42,
+            "config_list": config_list,
+            "max_retries" : 10
+        } 
+  
+def rag_chat():
+    _reset_agents()
+    groupchat = autogen.GroupChat(
+        agents=list(agents.values()), messages=[], max_round=12
+    )
+    manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=llm_config)
+
+    # Start chatting with boss_aid as this is the user proxy agent.
+    agents["compounderProxy"].initiate_chat(
+        manager,
+        problem=prompt,
+        n_results=3,
+    )
+
+rag_chat()
